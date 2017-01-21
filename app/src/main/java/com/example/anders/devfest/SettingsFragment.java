92ed0e6746374f9230de862b614f988ba96300ac
Anders <anders.wk.choi@gmail.com>
2017-01-21 21:41:29 +0900

    cancer

--- app/src/main/java/com/example/anders/devfest/SettingsFragment.java
+++ app/src/main/java/com/example/anders/devfest/SettingsFragment.java
@@ -1,6 +1,7 @@
 package com.example.anders.devfest;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,6 +9,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SettingsFragment extends Fragment {
 
@@ -16,7 +19,38 @@ public class SettingsFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        final TextView textView = (TextView)view.findViewById(R.id.textView);
+        SeekBar seekBar = (SeekBar)view.findViewById(R.id.seekBar);
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("stalin did nothing wrong", Context.MODE_PRIVATE);
+        int distance = sharedPreferences.getInt("distance", 50);
+        textView.setText(Integer.toString(distance));
+        seekBar.setProgress(distance - 50);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(Integer.toString(progress + 50));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int finalized = seekBar.getProgress() + 50;
+                SharedPreferences sharedPreferences = getActivity().getSharedPreferences("stalin did nothing wrong", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("distance", finalized);
+                editor.commit();
+            }
+        });
+
+        return view;
     }
 
 
