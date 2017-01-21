48a2c44cc9735e4a85f36f2437fafee0593b7422
Anders <anders.wk.choi@gmail.com>
2017-01-21 21:51:48 +0900

    cancer

--- app/src/main/java/com/example/anders/devfest/Room.java
+++ app/src/main/java/com/example/anders/devfest/Room.java
@@ -2,34 +2,44 @@ package com.example.anders.devfest;
 
 import android.location.Location;
 
+import java.util.ArrayList;
+
 /**
  * Created by q on 2017-01-21.
  */
 
 public class Room {
 
-    private String Name;
-    private int Size;
-    private String Description;
-    private Location Loca;
-    private String[] Members;
+    private String name;
+    private int capacity;
+    private String description;
+    private Location location;
+    private ArrayList<String> members;
 
-    public String GetName(){ return Name;}
-    public int GetSize(){return Size;}
-    public String GetDescription(){return Description;}
-    public Location GetLocation(){return Loca;}
-    public String[] Getmembers(){return Members;}
+    public String getName(){ return name;}
+    public int getCapacity() {return capacity;};
+    public int getSize(){return members.size();}
+    public String getDescription(){return description;}
+    public Location getLocation(){return location;}
+    public ArrayList<String> getMembers(){return members;}
 
-    public Room(){
-
+    public Room(String name,int capacity,String description,Location location, ArrayList<String> members){
+        this.name=name;
+        this.capacity=capacity;
+        this.description=description;
+        this.location=location;
+        this.members=members;
     }
 
-    public Room(String name,int size,String description,Location loca,String[] members){
-
-        this.Name=name;
-        this.Size=size;
-        this.Description=description;
-        this.Loca=loca;
-        this.Members=members;
+    public void addMember(String member) {
+        this.members.add(member);
+    }
+    public void removeMember(String member) {
+        for(int i = 0; i < this.members.size(); i++) {
+            if(this.members.get(i).equals(member)) {
+                this.members.remove(i);
+                return;
+            }
+        }
     }
 }
